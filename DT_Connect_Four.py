57f6b4a0449655ea4c6736540ac4fac2af1f47f9
Deivanai Thiyagarajan <42710609+DeivanaiThiyagarajan@users.noreply.github.com>
2024-10-05 00:43:40 -0400

    Add files via upload

--- DT_Connect_Four.py
+++ DT_Connect_Four.py
@@ -1,70 +1,104 @@
+'''
+Pseudcode:
+1. Initialize an empty board every time a new game starts
+2. print the empty board to the user
+3. iterate the plays for 42 times since we know there can be no more plays for a particular game
+4. first is player X and once an iteration completed swap between player between O and X
+5. Get a list of available positions in the board currently
+6. Get the input from the user for each play and validate the input.
+    If the input is not in the available list print error message and ask the user to enter the input again
+7. If values are correct check if the spot is open to play by checking if the row and column in the board is empty
+8. Place X or O in the selected spot and check for winning
+    - to find the correct spot check the column value and row value
+    - compare the column value with the number in the board array using Alpha_num_dict
+    - place the player in the column row value found
+9. For checking We check only 3 parts
+    a. the row in which the insert happened (not necessary to check other rows)
+    6. the column in which the insert happened (This check needs to happen if only the row value is greater than 2)
+    c. check the off diagonal for consecutive 4 same characters by decrementing row and column value by 1
+    b. check the main diagonal for the same by decrementing row value and incrementing column value
+10. If none of the above check results in full 4 count for X or O return False. Else return the winner and jump to step-13
+11. Print the winner if the returned value is true else continue by repeating steps (5-10)
+12. If all the 42 plays are done, Print Draw match
+13. Ask for a new game if yes repeat from step(1-12)
+'''
+
 class Connect_Four:
-    def __init__(self):
+    def __init__(self): # Initialize a new board whenever a new game is requested
         self.board = [['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','',''],['','','','','','','']]
-        self.num_alpha_dict = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g'}
+        #num-alpha dictionary to map number to column alphabet
+        self.num_alpha_dict = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g'} 
+        # alpha-num dictionary to convert alphabet to number to access the list (board)
         self.aplha_num_dict = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7}
-        #self.first_set_diagonals = [['a3','b4','c5','d6'],['a2','b3','c4','d5','e6'],['a1','b2','c3','d4','e5','f6'],['b1','c2','d3','e4','f5','g6'],['c1','d2','e3','f4','g5'],['d1','e2','f3','g4']]
-        #self.opposite_set_diagonals = {3:['30','21','12','03'],4:['40','31','22','13','04'],5:['50','41','32','23','14','05'],6:['51','42','33','24','15','06'],7:['52','43','34','25','16'],8:['53','44','35','26']}
     def print_board(self):
+        # print the board values
         print('---------------------------------')
         for i in range(6,0,-1):
             print('| '+ str(i),end=' | ')
             for j in range(7):
-                if(self.board[i-1][j]==''):
-                    print(' ',end = ' | ')
+                if(self.board[i-1][j]==''): #check for empty spot
+                    print(' ',end = ' | ') 
                 else:
                     print(self.board[i-1][j],end=' | ')
             print()
             print('---------------------------------')
+        # print the board constants
         print('|R/C| a | b | c | d | e | f | g |')
         print('---------------------------------')
     def available_positions(self):
+        #once the function is called check the currently available positions in the board
         l = []
-        #alpha_num_dict = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g'}
-        for i in range(7):
+        for i in range(7): #check for all columns
             for j in range(6):
                 if(self.board[j][i]==''):
                     l.append(self.num_alpha_dict[i+1]+str(j+1))
-                    break
+                    break #break searching the other rows if eg: 0 spot is empty for a column
         return l
     def check_move(self,move,available):
-        if(move[0] not in ('a','b','c','d','e','f','g') or move[1] not in ('1','2','3','4','5','6')):
+        #check if the move is correct by checking if the first character input is between a-g
+        #check if the second character is between 1-6
+        if(len(move)!=2 or move[0] not in ('a','b','c','d','e','f','g') or move[1] not in ('1','2','3','4','5','6')):
             return 'incorrect'
-        elif(move not in available):
+        elif(move not in available): # check if the spot is available list
             return 'unavailable'
-        else:
+        else: #return as correct move
             return 'correct'
         
     def play_move(self,move,player):
+        #Find the column numeric index by using the alpha_num_dictionary
         column = self.aplha_num_dict[move[0]]-1
+        #subtract nrow number input by -1 since the input user enters is 1-indexed but list is 0-indexed
         row = int(move[1])-1
-        #print(number)
+        #place the player in the respective row an column in the board
         self.board[row][column] = player
         return
     
     def __validate(self,listofRecords,player):
         count = 0
-        for i in listofRecords:
+        for i in listofRecords: #get the current list to check
             if(i==player):
                 count+=1
-                if(count==4):
+                if(count==4): #check if the count is 4 if so return true for winner
                     return True
             else:
-                count = 0
+                count = 0 #assign count to 0 if any inbetween value is another player
         if(count==4):
-            return True
+            return True #if count reaches four return true else false
         return False
     
     def validate_move(self,move,player):
+        #Find the column numeric index by using the alpha_num_dictionary
         column = self.aplha_num_dict[move[0]]-1
+        #subtract nrow number input by -1 since the input user enters is 1-indexed but list is 0-indexed
         row = int(move[1])-1
-        listrow = self.board[row]
+        listrow = self.board[row] 
+        #check the current column that the user has placed his move in
         result = self.__validate(listrow,player)
         if(result == True):
             return player + ' is the Winner!'
-        if(row>2):
+        if(row>2): #check for column and dioagonal winner only if the row is greater than 2
             listcolumn = []
-            for i in range(6):
+            for i in range(6): #check the current column
                 listcolumn.append(self.board[i][column])
             result = self.__validate(listcolumn,player)
             if(result == True):
@@ -73,7 +107,7 @@ class Connect_Four:
             listdiagonal1 = []
             i=row
             j=column
-            while(i>=0 and j>=0):
+            while(i>=0 and j>=0): # check the off-diagonal
                 listdiagonal1.append(self.board[i][j])
                 i-=1
                 j-=1
@@ -84,7 +118,7 @@ class Connect_Four:
             listdiagonal2=[]
             i=row
             j=column
-            while(i>=0 and j<=6):
+            while(i>=0 and j<=6): # check the main diagonal
                 listdiagonal2.append(self.board[i][j])
                 i-=1
                 j+=1
@@ -97,48 +131,49 @@ class Connect_Four:
         
 
 def main():
-    choice = 'y'
+    choice = 'y' # Initial choice is yes for atleast one game in the start
     while(choice.lower() in ('y','yes')):
         print('New game: X goes first.')
-        new_game = Connect_Four()
-        new_game.print_board()
+        new_game = Connect_Four() #Initialize the board
+        new_game.print_board() # print the empty board to the user to ask input
         player = 'X'
         complete = False
         for i in range(42):
             print(player+"'s turn.")
             print('Where do you want your '+player+' placed?')
-            available = new_game.available_positions()
+            available = new_game.available_positions() #get the list of available positions
             print('Available positions are:',available)
             print()
-            incorrect_move = True
+            incorrect_move = True #initiallt incorrect move is true to get the input
             while(incorrect_move):
                 move = input('Please enter column-letter and row-number (e.g., a1):')
-                check_move = new_game.check_move(move,available)
-                if(check_move=='incorrect'):
+                check_move = new_game.check_move(move,available) #check the move of the player
+                if(check_move=='incorrect'): #print error message if incorrect move and ask the new input again
                     print('Invalid entry: try again.')
-                elif(check_move == 'unavailable'):
+                elif(check_move == 'unavailable'): #print error message if move not available and ask for new input
                     print('That cell is already taken.')
                     print('Please make another selection.')
                 else:
                     incorrect_move = False
-            new_game.play_move(move,player)
-            if(i>5):
-                result = new_game.validate_move(move,player)
-                if(result != None):
+            new_game.play_move(move,player) #place the player in the position mentioned
+            if(i>5): #check if atleast 5 moves passed as there cannot be winner less than 5 moves
+                result = new_game.validate_move(move,player) #validate the move for winner
+                if(result != None): #if result is not none then there is a winner print the message
                     print(result)
-                    complete = True
-                    new_game.print_board()
-                    break
-            if(player == 'X'):
+                    complete = True # set complete to true to end the game
+                    new_game.print_board() #print the final board
+                    break #break this game
+            if(player == 'X'): #swap between the players
                 player = 'O'
             else:
                 player = 'X'
             if(i<42):
-                new_game.print_board()
-        if(complete==0): #if there is no winner print the board
+                new_game.print_board() #if current turn is less than 42 print the current state of the board
+        if(complete==0): #if there is no winner print the board and display draw game message
             print('Draw! NOBODY WINS!')
             new_game.print_board()
-        choice = input('Another game (y/n)?')
+        choice = input('Another game (y/n)?') #Ask the user for a new game
     print('Thank you for playing!')
 
-main()
+if __name__ == '__main__':
+    main()
--- DT_Tic_Tac_Toe.py
+++ DT_Tic_Tac_Toe.py
@@ -5,7 +5,8 @@ Pseudcode:
 3. iterate the plays for 9 times since we know there can be no more plays for a particular game
 4. first is player X and once an iteration completed swap between player between O and X
 5. Get the input from the user for each play and validate the input.
-    If value<0 or >2 print error message
+    If value does not contain 3 characters and seperated by comma 
+    and first and last character are not in (0,1,2) print error message
 6. If values are correct check if the spot is open to play by checking if the row and column in the board is empty
 7. Place X or O in the selected spot and check for winning
 8. We check only 3 parts
@@ -13,115 +14,119 @@ Pseudcode:
     6. the column in which the insert happened
     c. If both the row and column values are equal check the first diagonal
     b. if adding both row and column gives 2(n-1) check the opposite diagonal
-7. If none of the above check results in full 3 count for X or O return False. Else return the winner and jump to step-10
-8. Print the winner if the returned value is true else continue by repeating steps (5-7)
-9. If all the 9 plays are done, Print Draw match
-10. Ask for a new game if yes repeat from step(1-10)
+9. If none of the above check results in full 3 count for X or O return False. Else return the winner and jump to step-12
+10. Print the winner if the returned value is true else continue by repeating steps (5-8)
+11. If all the 9 plays are done, Print Draw match
+12. Ask for a new game if yes repeat from step(1-11)
 '''
 
 
-class TicTacToe:
-    def __init__(self):
-        #initialize an empty board everytime a new game starts
-        self.board = [['','',''],['','',''],['','','']]
-    def print_board(self):
-        #board constants that that doesnt change for each play
-        print('-------------------')
-        print('| R\C | 0 | 1 | 2 |')
-        print('-------------------')
+
+def print_board(board):
+        #board constants that doesnt change for each play
+    print('-------------------')
+    print('| R\\C | 0 | 1 | 2 |')
+    print('-------------------')
         #print the values stored in board for each game
-        for i in range(3):
-            print('| '+str(i).ljust(3),end = ' | ')
-            for j in range(3):
-                if(self.board[i][j]==''): #if empty spot print empty space
-                    print(' ',end = ' | ') 
-                else:
-                    print(self.board[i][j],end = ' | ')
-            print()
-            print('-------------------')
-        return 
-    
-    def Get_checkavailability(self):
-        available = False # Initially availability is false
-        while(not(available)):
-            r,c = map(int,input('Please enter row number and column number separated by a comma.').split(','))
-            while(r<0 or r>2 or c<0 or c>2): # If incorrect values provided get input
-                print('Invalid entry: try again.')
-                print('Row & column numbers must be either 0, 1, or 2.')
-                r,c = map(int,input('Please enter row number and column number separated by a comma.').split(','))
-            # check if the space is occupied
-            if(self.board[r][c]!=''):
-                available = False
+    for i in range(3):
+        print('| '+str(i).ljust(3),end = ' | ')
+        for j in range(3):
+            if(board[i][j]==''): #if empty spot print empty space
+                print(' ',end = ' | ') 
             else:
-                available = True
-            if(not(available)):
-                print('That cell is already taken.')
-                print('Please make another selection.')
+                print(board[i][j],end = ' | ')
+        print()
+        print('-------------------')
+    return 
+    
+def Get_checkavailability(board):
+    available = False # Initially availability is false
+    valid_inputs = ['1','2','0']
+    while(not(available)):
+        inputs = input('Please enter row number and column number separately by a comma.')
+        while(len(inputs)!=3 or inputs[0] not in valid_inputs or inputs[1]!=',' or inputs[2] not in valid_inputs):
+            print('Invalid entry: try again.')
+            print('Row & column numbers must be either 0, 1, or 2.')
+            inputs = input('Please enter row number and column number separately by a comma.')
+            #r,c = map(int,input('Please enter row number and column number separated by a comma.').split(','))
+        r,c = map(int,inputs.split(','))
+            #while(r<0 or r>2 or c<0 or c>2): # If incorrect values provided get input
+                #print('Invalid entry: try again.')
+                #print('Row & column numbers must be either 0, 1, or 2.')
+                #r,c = map(int,input('Please enter row number and column number separated by a comma.').split(','))
+            # check if the space is occupied
+        if(board[r][c]!=''):
+            available = False
+        else:
+            available = True
+        if(not(available)):
+            print('That cell is already taken.')
+            print('Please make another selection.')
         #if all tests are passed display the input
-        print('You have entered row #'+str(r))
-        print('\t\t and column #'+str(c))
-        return (r,c)
+    print('You have entered row #'+str(r))
+    print('\t\t and column #'+str(c))
+    return (r,c)
         
-    def __checkcomplete(self,pointer,flag,player):
+def __checkcomplete(board,pointer,flag,player):
         #if flag is r check the particular row for the count of the player
-        if(flag=='r'):
-            if(self.board[pointer].count(player)==3):
-                return True
-            return False
+    if(flag=='r'):
+        if(board[pointer].count(player)==3):
+            return True
+        return False
         #if flag is 'c' check the particular column for the count of the player
-        if(flag=='c'):
+    if(flag=='c'):
+        for i in range(3):
+            if(board[i][pointer]!=player):
+                return False
+        return True
+        #if the flag is 'd' check the diagonals for the count of the player
+    if(flag=='d'):
+        if(pointer==0):#for 1st diagonal
             for i in range(3):
-                if(self.board[i][pointer]!=player):
+                if(board[i][i]!=player):
                     return False
             return True
-        #if the flag is 'd' check the diagonals for the count of the player
-        if(flag=='d'):
-            if(pointer==0):#for 1st diagonal
-                for i in range(3):
-                    if(self.board[i][i]!=player):
-                        return False
-                return True
-            else:#for opposite diagonal
-                for i in range(3):
-                    if(self.board[2-i][i]!=player):
-                        return False
-                return True
+        else:#for opposite diagonal
+            for i in range(3):
+                if(board[2-i][i]!=player):
+                    return False
+            return True
                     
-    def fill_checkwin(self,r,c,player):
-        self.board[r][c] = player #place the current player in the board
-        res = self.__checkcomplete(r,'r',player) # check for row match
+def fill_checkwin(board,r,c,player):
+    board[r][c] = player #place the current player in the board
+    res = __checkcomplete(board,r,'r',player) # check for row match
+    if(res==True):
+        return player + ' is the Winner!!!'
+    res = __checkcomplete(board,c,'c',player) # check for column match
+    if(res==True):
+        return player + ' is the Winner!!!'
+    if(r==c): # check for first diagonal match
+        res = __checkcomplete(board,0,'d',player)
         if(res==True):
             return player + ' is the Winner!!!'
-        res = self.__checkcomplete(c,'c',player) # check for column match
+    if((r+c)==(2)): #check for opposite diagonal match
+        res = __checkcomplete(board,1,'d',player)
         if(res==True):
             return player + ' is the Winner!!!'
-        if(r==c): # check for first diagonal match
-            res = self.__checkcomplete(0,'d',player)
-            if(res==True):
-                return player + ' is the Winner!!!'
-        if((r+c)==(2)): #check for opposite diagonal match
-            res = self.__checkcomplete(1,'d',player)
-            if(res==True):
-                return player + ' is the Winner!!!'
-        return
+    return
 
 def main():
     choice = 'y'
     while(choice.lower() in ('y','yes')):
-        new_game = TicTacToe() #Initialize a new game
+        board = [['','',''],['','',''],['','','']] #Initialize a new game
         print('New Game: X goes first.')
-        new_game.print_board()
+        print_board(board)
         player = 'X'
         Complete = 0
         for i in range(9): # each game consist of 9 plays
             print(player+"'s turn.")
             print('Where do you want your '+ player +' placed?')
-            r,c = new_game.Get_checkavailability() # get input from user and validate the input
+            r,c = Get_checkavailability(board) # get input from user and validate the input
             print('Thank you for your selection.')
-            result = new_game.fill_checkwin(r,c,player) # fill the selection and check for win
+            result = fill_checkwin(board,r,c,player) # fill the selection and check for win
             if(result != None):#if there is a win print the winner and break the play
                 print(result)
-                new_game.print_board()
+                print_board(board)
                 Complete = 1
                 break
             #swap the player after each turn
@@ -130,12 +135,13 @@ def main():
             else:
                 player = 'O'
             if(i < 8): #print the board only 8 times since the last play print is happening outside
-                new_game.print_board()
+                print_board(board)
         if(Complete==0): #if there is no winner print the board
             print('Draw! NOBODY WINS!')
-            new_game.print_board()
+            print_board(board)
         #ask the user for a new game
         choice = input('Another game? Enter Y or y for yes.')
     print('Thank you for Playing!')
 
-main()
+if __name__ == '__main__':
+    main()
